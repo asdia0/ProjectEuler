b6b4ed2778a5651d6f553fa7059db27a1fc133ad
asdia0 <asdia.the.potato@gmail.com>
2021-03-10 17:40:12 +0800

    made my life easier

---
+++ src/Project Euler/Solutions/.Template.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectEuler
+{
+    using System;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=N">Project Euler: Problem N</see>
+        /// </summary>
+        /// <returns>Answer</returns>
+        public static void ProblemN()
+        {
+        }
+    }
+}
