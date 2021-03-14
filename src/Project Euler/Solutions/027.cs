49fc38611831d8543df20c40bfa17391e9f76735
asdia0 <asdia.the.potato@gmail.com>
2021-03-14 23:26:27 +0800

    solved q27

---
+++ src/Project Euler/Solutions/027.cs
@@ -0,0 +1,42 @@
+﻿namespace ProjectEuler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=27">Project Euler: Problem 27</see>
+        /// </summary>
+        /// <returns>Answer</returns>
+        public static long Problem27()
+        {
+            Dictionary<(int, int), int> d = new Dictionary<(int, int), int>();
+
+            for (int a = -999; a < 1000; a++)
+            {
+                for (int b = 0; b <= 1000; b++)
+                {
+                    if (!IsPrime(b))
+                    {
+                        continue;
+                    }
+
+                    int n = 0;
+
+                    while (IsPrime(Math.Abs((n * n) + (a * n) + b)))
+                    {
+                        n++;
+                    }
+
+                    d.Add((a, b), n);
+                }
+            }
+
+            (int aSol, int bSol) = d.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+
+            return aSol * bSol;
+        }
+    }
+}
