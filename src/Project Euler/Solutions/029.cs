6647d6cfa875348c12aad8fbf8deb1c03f6a77a2
asdia0 <asdia.the.potato@gmail.com>
2021-03-15 00:51:31 +0800

    solve q29

---
+++ src/Project Euler/Solutions/029.cs
@@ -0,0 +1,29 @@
+﻿namespace ProjectEuler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=29">Project Euler: Problem 29</see>
+        /// </summary>
+        /// <returns>Answer</returns>
+        public static long Problem29()
+        {
+            HashSet<BigInteger> l = new HashSet<BigInteger>();
+
+            for (int a = 2; a <= 100; a++)
+            {
+                for (int b = 2; b <= 100; b++)
+                {
+                    l.Add(BigInteger.Pow(a, b));
+                }
+            }
+
+            return l.Count;
+        }
+    }
+}
