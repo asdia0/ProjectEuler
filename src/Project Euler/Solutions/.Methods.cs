cbea012c7d66d2a682bafd3ee9b3e34693fd27e4
asdia0 <asdia.the.potato@gmail.com>
2021-03-15 02:14:56 +0800

    solved q33

--- src/Project Euler/Solutions/.Methods.cs
+++ src/Project Euler/Solutions/.Methods.cs
@@ -117,5 +117,18 @@
 
             return false;
         }
+
+        public static long GCD(long a, long b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a | b;
+        }
     }
 }
---
+++ src/Project Euler/Solutions/033.cs
@@ -0,0 +1,35 @@
+﻿namespace ProjectEuler
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=33">Project Euler: Problem 33</see>
+        /// </summary>
+        /// <returns>100</returns>
+        public static long Problem33()
+        {
+            int aProduct = 1;
+            int bProduct = 1;
+
+            for (int i = 1; i < 10; i++)
+            {
+                for (int a = 1; a < i; a++)
+                {
+                    for (int b = 1; b < a; b++)
+                    {
+                        if ((b * 10 + i) * a == b * (i * 10 + a))
+                        {
+                            aProduct *= a;
+                            bProduct *= b;
+                        }
+                    }
+                }
+            }
+
+            return aProduct / GCD(aProduct, bProduct);
+        }
+    }
+}
