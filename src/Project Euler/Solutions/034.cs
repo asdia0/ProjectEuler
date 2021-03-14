a17263727c615aad4b546a983bb5e957edc85724
asdia0 <asdia.the.potato@gmail.com>
2021-03-15 02:21:25 +0800

    solved q34

---
+++ src/Project Euler/Solutions/034.cs
@@ -0,0 +1,33 @@
+﻿namespace ProjectEuler
+{
+    using System;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=34">Project Euler: Problem 34</see>
+        /// </summary>
+        /// <returns>40730</returns>
+        public static long Problem34()
+        {
+            long sum = 0;
+
+            for (int i = 10; i < 999999; i++)
+            {
+                long factSum = 0;
+
+                foreach (int digit in GetDigits(i))
+                {
+                    factSum += (long)Factorial(digit);
+                }
+
+                if (factSum == i)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
