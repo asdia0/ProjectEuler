18907b30d6ce20e9bf15b2258c5cd0c3987232c1
asdia0 <asdia.the.potato@gmail.com>
2021-03-15 14:07:47 +0800

    solved q40

---
+++ src/Project Euler/Solutions/040.cs
@@ -0,0 +1,30 @@
+﻿namespace ProjectEuler
+{
+    using System;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=40">Project Euler: Problem 40</see>
+        /// </summary>
+        /// <returns>210</returns>
+        public static long Problem40()
+        {
+            string s = "0";
+            int i = 0;
+
+            while (s.Length < 1000001)
+            {
+                i++;
+                s += i;
+            }
+
+            return int.Parse(s[10].ToString()) *
+                int.Parse(s[100].ToString()) *
+                int.Parse(s[1000].ToString()) *
+                int.Parse(s[10000].ToString()) *
+                int.Parse(s[100000].ToString()) *
+                int.Parse(s[1000000].ToString());
+        }
+    }
+}
