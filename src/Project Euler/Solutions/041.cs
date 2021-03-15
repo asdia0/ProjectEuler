387013ed9c461511013748195f2764c4ad962cfc
asdia0 <asdia.the.potato@gmail.com>
2021-03-15 14:12:58 +0800

    solved q41

---
+++ src/Project Euler/Solutions/041.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectEuler
+{
+    using System;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=41">Project Euler: Problem 41</see>
+        /// </summary>
+        /// <returns>7652413</returns>
+        public static long Problem41()
+        {
+            for (int i = 7654321; i > 0; i--)
+            {
+                if (IsPandigital(i) && IsPrime(i))
+                {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
