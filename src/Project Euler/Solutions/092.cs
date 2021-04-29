5c56ee13025bc79d5bf5c1b61334593ef632c605
asdia0 <asdia.the.potato@gmail.com>
2021-04-29 22:03:02 +0800

    solved q92

---
+++ src/Project Euler/Solutions/092.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjectEuler
+{
+    using System;
+
+    public partial class Solutions
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=92">Project Euler: Problem 92</see>
+        /// </summary>
+        /// <returns>Answer</returns>
+        public static long Problem92()
+        {
+            long count = 0;
+
+            for (int n = 1; n <= 10000000; n++)
+            {
+                if (n % 1000000 == 0)
+                {
+                    Console.WriteLine(n);
+                }
+
+                if (_092_Sum(n) == 89)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static long _092_Sum(long n)
+        {
+            int sum = 0;
+
+            foreach (int digit in GetDigits(n))
+            {
+                sum += digit * digit;
+            }
+
+            if (sum == 1 || sum == 89)
+            {
+                return sum;
+            }
+
+            return _092_Sum(sum);
+        }
+    }
+}
